e5a21076d1ed0649f99bc3fd2c41c62a221fa118
kimyounghee425 <doctorsean0425@gmail.com>
2026-02-01 14:40:09 +0900

    ✨ FEAT : 카테고리 상수 추가

---
+++ apps/web/src/shared/constants/category.ts
@@ -0,0 +1,9 @@
+export const CATEGORIES = [
+  { id: 1, name: '한식' },
+  { id: 2, name: '양식' },
+  { id: 3, name: '일식' },
+  { id: 4, name: '카페' },
+  { id: 5, name: '데이트' },
+  { id: 6, name: '이자카야' },
+  { id: 7, name: '기타' },
+] as const;
